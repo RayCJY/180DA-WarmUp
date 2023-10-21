ea3e0ed34e706615426974ed4181ec3549de4407
Ray <raybruin88@g.ucla.edu>
2023-10-21 03:22:31 +0100

    lab2

---
+++ lab2/test.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == '__main__':
+    x = input(f"Which course are you taking?")
+    if x == "ECE 180":
+        print(x + " is the best class ever")
+    else:
+        print("Bruins are the best!")
+        print("Have a nice day")
